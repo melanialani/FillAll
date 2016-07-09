ea03b88771271a17717cea004de38d9dcc9f0712
Melania Laniwati <melanialani@gmail.com>
2016-07-09 12:31:45 +0700

    stage 1 done

--- app/src/main/java/com/application/melanialani/fillall/MainActivity.java
+++ app/src/main/java/com/application/melanialani/fillall/MainActivity.java
@@ -1,6 +1,5 @@
 package com.application.melanialani.fillall;
 
-import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,6 +7,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.github.pwittchen.swipe.library.Swipe;
 import com.github.pwittchen.swipe.library.SwipeListener;
@@ -23,10 +23,13 @@ public class MainActivity extends AppCompatActivity {
 
     private int             posX, posY;
 
-    private static final int lebar = 4;
-    private static final int tinggi = 5;
+    private int             lebar = 4;
+    private int             tinggi = 5;
+
+    private Stage1          stage1;
     //endregion
 
+    //region dont touch this if not necessary
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,40 +53,6 @@ public class MainActivity extends AppCompatActivity {
     @Override public boolean dispatchTouchEvent(MotionEvent event) {
         swipe.dispatchTouchEvent(event);
         return super.dispatchTouchEvent(event);
-    }
-
-    private void defineImageView() {
-        mapspict[0][0] = (ImageView) findViewById(R.id.iv00);   mapspict[1][0] = (ImageView) findViewById(R.id.iv10);
-        mapspict[0][1] = (ImageView) findViewById(R.id.iv01);   mapspict[1][1] = (ImageView) findViewById(R.id.iv11);
-        mapspict[0][2] = (ImageView) findViewById(R.id.iv02);   mapspict[1][2] = (ImageView) findViewById(R.id.iv12);
-        mapspict[0][3] = (ImageView) findViewById(R.id.iv03);   mapspict[1][3] = (ImageView) findViewById(R.id.iv13);
-
-        mapspict[2][0] = (ImageView) findViewById(R.id.iv20);   mapspict[3][0] = (ImageView) findViewById(R.id.iv30);
-        mapspict[2][1] = (ImageView) findViewById(R.id.iv21);   mapspict[3][1] = (ImageView) findViewById(R.id.iv31);
-        mapspict[2][2] = (ImageView) findViewById(R.id.iv22);   mapspict[3][2] = (ImageView) findViewById(R.id.iv32);
-        mapspict[2][3] = (ImageView) findViewById(R.id.iv23);   mapspict[3][3] = (ImageView) findViewById(R.id.iv33);
-
-        mapspict[4][0] = (ImageView) findViewById(R.id.iv40);
-        mapspict[4][1] = (ImageView) findViewById(R.id.iv41);
-        mapspict[4][2] = (ImageView) findViewById(R.id.iv42);
-        mapspict[4][3] = (ImageView) findViewById(R.id.iv43);
-
-        // empty array maps and hide imageview
-        for (int x = 0; x < tinggi; x++){
-            for (int y = 0; y < lebar; y++){
-                try {
-                    maps[x][y] = "0";
-                } catch (Exception ex){
-                    Log.e("array_ERR", ex.toString());
-                }
-            }
-        }
-
-        // set default player position
-        posX = 4;
-        posY = 3;
-        maps[posX][posY] = "1";
-        mapspict[posX][posY].setImageResource(R.drawable.all);
     }
 
     private void swipeDetector() {
@@ -133,56 +102,60 @@ public class MainActivity extends AppCompatActivity {
 
     private void move(String action) {
         if (action.equalsIgnoreCase("RIGHT")){ // x same, y plus
-            int posYbefore = posY;
-            for (int y = posYbefore; y < lebar; y++){
+            int posYwanted = posY + 1;
+            for (int y = posYwanted; y < lebar; y++){
                 try {
                     if (maps[posX][y].equals("0")){
                         maps[posX][y] = "1";
                         mapspict[posX][y].setImageResource(R.drawable.all);
                         posY = y;
-                    }
+                    } else
+                        break;
                 } catch (Exception ex){
                     Log.e("move_ERR", ex.toString());
                 }
                 //textView.setText(textView.getText() + " " + posX + "," + posY);
             }
         } else if (action.equalsIgnoreCase("LEFT")){ // x same, y minus
-            int posYbefore = posY;
-            for (int y = posYbefore; y >= 0; y--){
+            int posYwanted = posY - 1;
+            for (int y = posYwanted; y >= 0; y--){
                 try {
                     if (maps[posX][y].equals("0")){
                         maps[posX][y] = "1";
                         mapspict[posX][y].setImageResource(R.drawable.all);
                         posY = y;
-                    }
+                    } else
+                        break;
                 } catch (Exception ex){
                     Log.e("move_ERR", ex.toString());
                 }
                 //textView.setText(textView.getText() + " " + posX + "," + posY);
             }
         } else if (action.equalsIgnoreCase("DOWN")){ // x plus, y same
-            int posXbefore = posX;
-            for (int x = posXbefore; x < tinggi; x++){
+            int posXwanted = posX + 1;
+            for (int x = posXwanted; x < tinggi; x++){
                 try {
                     if (maps[x][posY].equals("0")){
                         maps[x][posY] = "1";
                         mapspict[x][posY].setImageResource(R.drawable.all);
                         posX = x;
-                    }
+                    } else
+                        break;
                 } catch (Exception ex){
                     Log.e("move_ERR", ex.toString());
                 }
                 //textView.setText(textView.getText() + " " + posX + "," + posY);
             }
         } else if (action.equalsIgnoreCase("UP")){ // x minus, y same
-            int posXbefore = posX;
-            for (int x = posXbefore; x >= 0; x--){
+            int posXwanted = posX - 1;
+            for (int x = posXwanted; x >= 0; x--){
                 try {
                     if (maps[x][posY].equals("0")){
                         maps[x][posY] = "1";
                         mapspict[x][posY].setImageResource(R.drawable.all);
                         posX = x;
-                    }
+                    } else
+                        break;
                 } catch (Exception ex){
                     Log.e("move_ERR", ex.toString());
                 }
@@ -190,4 +163,189 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
+    //endregion
+
+    private void defineImageView() {
+        mapspict[0][0] = (ImageView) findViewById(R.id.iv00);   mapspict[1][0] = (ImageView) findViewById(R.id.iv10);
+        mapspict[0][1] = (ImageView) findViewById(R.id.iv01);   mapspict[1][1] = (ImageView) findViewById(R.id.iv11);
+        mapspict[0][2] = (ImageView) findViewById(R.id.iv02);   mapspict[1][2] = (ImageView) findViewById(R.id.iv12);
+        mapspict[0][3] = (ImageView) findViewById(R.id.iv03);   mapspict[1][3] = (ImageView) findViewById(R.id.iv13);
+
+        mapspict[2][0] = (ImageView) findViewById(R.id.iv20);   mapspict[3][0] = (ImageView) findViewById(R.id.iv30);
+        mapspict[2][1] = (ImageView) findViewById(R.id.iv21);   mapspict[3][1] = (ImageView) findViewById(R.id.iv31);
+        mapspict[2][2] = (ImageView) findViewById(R.id.iv22);   mapspict[3][2] = (ImageView) findViewById(R.id.iv32);
+        mapspict[2][3] = (ImageView) findViewById(R.id.iv23);   mapspict[3][3] = (ImageView) findViewById(R.id.iv33);
+
+        mapspict[4][0] = (ImageView) findViewById(R.id.iv40);
+        mapspict[4][1] = (ImageView) findViewById(R.id.iv41);
+        mapspict[4][2] = (ImageView) findViewById(R.id.iv42);
+        mapspict[4][3] = (ImageView) findViewById(R.id.iv43);
+
+        // empty array maps
+        for (int x = 0; x < tinggi; x++){
+            for (int y = 0; y < lebar; y++){
+                try {
+                    maps[x][y] = "0";
+                } catch (Exception ex){
+                    Log.e("array_ERR", ex.toString());
+                }
+            }
+        }
+
+        // set default player position
+        posX = 4;
+        posY = 3;
+        maps[posX][posY] = "1";
+        mapspict[posX][posY].setImageResource(R.drawable.all);
+    }
+
+    //region stage1
+    public void level1(View v){
+        // get data from stage1
+        stage1 = new Stage1();
+        stage1.setLevel(1);
+
+        initiateNewMap();
+    }
+
+    public void level2(View v){
+        // get data from stage1
+        stage1 = new Stage1();
+        stage1.setLevel(2);
+
+        initiateNewMap();
+    }
+
+    public void level3(View v){
+        // get data from stage1
+        stage1 = new Stage1();
+        stage1.setLevel(3);
+
+        initiateNewMap();
+    }
+
+    public void level4(View v){
+        // get data from stage1
+        stage1 = new Stage1();
+        stage1.setLevel(4);
+
+        initiateNewMap();
+    }
+
+    public void level5(View v){
+        // get data from stage1
+        stage1 = new Stage1();
+        stage1.setLevel(5);
+
+        initiateNewMap();
+    }
+    //endregion
+
+    private void initiateNewMap(){
+        this.lebar = stage1.getLebar();
+        this.tinggi = stage1.getTinggi();
+        this.posX = stage1.getPosX();
+        this.posY = stage1.getPosY();
+
+        this.maps = new String[tinggi][lebar];
+        this.maps = stage1.getMap();
+
+        this.mapspict = new ImageView[tinggi][lebar];
+
+        // set layout & put each image view into array
+        if (tinggi == 1 && lebar == 4) {
+            setContentView(R.layout.map1x4);
+            defineMap1x4();
+        } else if (tinggi == 4 && lebar == 4) {
+            setContentView(R.layout.map4x4);
+            defineMap4x4();
+        } else if (tinggi == 5 && lebar == 5) {
+            setContentView(R.layout.map5x5);
+            defineMap5x5();
+        } else if (tinggi == 5 && lebar == 6) {
+            setContentView(R.layout.map5x6);
+            defineMap5x6();
+        }
+
+        // properly set image source in each image view
+        for (int x = 0; x < tinggi; x++){
+            for (int y = 0; y < lebar; y++){
+                try {
+                    if (maps[x][y].equals("0")){
+                        mapspict[x][y].setImageResource(R.drawable.blank);
+                    } else if (maps[x][y].equals("1")){
+                        mapspict[x][y].setImageResource(R.drawable.all);
+                    } else if (maps[x][y].equals("#")){
+                        mapspict[x][y].setImageResource(R.drawable.block);
+                    }
+                } catch (Exception ex){
+                    Log.e("array_ERR", ex.toString());
+                }
+            }
+        }
+    }
+
+    //region define array image view
+    private void defineMap1x4(){
+        mapspict[0][0] = (ImageView) findViewById(R.id.iv00);
+        mapspict[0][1] = (ImageView) findViewById(R.id.iv01);
+        mapspict[0][2] = (ImageView) findViewById(R.id.iv02);
+        mapspict[0][3] = (ImageView) findViewById(R.id.iv03);
+    }
+
+    private void defineMap4x4(){
+        mapspict[0][0] = (ImageView) findViewById(R.id.iv00);   mapspict[1][0] = (ImageView) findViewById(R.id.iv10);
+        mapspict[0][1] = (ImageView) findViewById(R.id.iv01);   mapspict[1][1] = (ImageView) findViewById(R.id.iv11);
+        mapspict[0][2] = (ImageView) findViewById(R.id.iv02);   mapspict[1][2] = (ImageView) findViewById(R.id.iv12);
+        mapspict[0][3] = (ImageView) findViewById(R.id.iv03);   mapspict[1][3] = (ImageView) findViewById(R.id.iv13);
+
+        mapspict[2][0] = (ImageView) findViewById(R.id.iv20);   mapspict[3][0] = (ImageView) findViewById(R.id.iv30);
+        mapspict[2][1] = (ImageView) findViewById(R.id.iv21);   mapspict[3][1] = (ImageView) findViewById(R.id.iv31);
+        mapspict[2][2] = (ImageView) findViewById(R.id.iv22);   mapspict[3][2] = (ImageView) findViewById(R.id.iv32);
+        mapspict[2][3] = (ImageView) findViewById(R.id.iv23);   mapspict[3][3] = (ImageView) findViewById(R.id.iv33);
+    }
+
+    private void defineMap5x5(){
+        mapspict[0][0] = (ImageView) findViewById(R.id.iv00);   mapspict[1][0] = (ImageView) findViewById(R.id.iv10);
+        mapspict[0][1] = (ImageView) findViewById(R.id.iv01);   mapspict[1][1] = (ImageView) findViewById(R.id.iv11);
+        mapspict[0][2] = (ImageView) findViewById(R.id.iv02);   mapspict[1][2] = (ImageView) findViewById(R.id.iv12);
+        mapspict[0][3] = (ImageView) findViewById(R.id.iv03);   mapspict[1][3] = (ImageView) findViewById(R.id.iv13);
+        mapspict[0][4] = (ImageView) findViewById(R.id.iv04);   mapspict[1][4] = (ImageView) findViewById(R.id.iv14);
+
+        mapspict[2][0] = (ImageView) findViewById(R.id.iv20);   mapspict[3][0] = (ImageView) findViewById(R.id.iv30);
+        mapspict[2][1] = (ImageView) findViewById(R.id.iv21);   mapspict[3][1] = (ImageView) findViewById(R.id.iv31);
+        mapspict[2][2] = (ImageView) findViewById(R.id.iv22);   mapspict[3][2] = (ImageView) findViewById(R.id.iv32);
+        mapspict[2][3] = (ImageView) findViewById(R.id.iv23);   mapspict[3][3] = (ImageView) findViewById(R.id.iv33);
+        mapspict[2][4] = (ImageView) findViewById(R.id.iv24);   mapspict[3][4] = (ImageView) findViewById(R.id.iv34);
+
+        mapspict[4][0] = (ImageView) findViewById(R.id.iv40);
+        mapspict[4][1] = (ImageView) findViewById(R.id.iv41);
+        mapspict[4][2] = (ImageView) findViewById(R.id.iv42);
+        mapspict[4][3] = (ImageView) findViewById(R.id.iv43);
+        mapspict[4][4] = (ImageView) findViewById(R.id.iv44);
+    }
+
+    private void defineMap5x6(){
+        mapspict[0][0] = (ImageView) findViewById(R.id.iv00);   mapspict[1][0] = (ImageView) findViewById(R.id.iv10);
+        mapspict[0][1] = (ImageView) findViewById(R.id.iv01);   mapspict[1][1] = (ImageView) findViewById(R.id.iv11);
+        mapspict[0][2] = (ImageView) findViewById(R.id.iv02);   mapspict[1][2] = (ImageView) findViewById(R.id.iv12);
+        mapspict[0][3] = (ImageView) findViewById(R.id.iv03);   mapspict[1][3] = (ImageView) findViewById(R.id.iv13);
+        mapspict[0][4] = (ImageView) findViewById(R.id.iv04);   mapspict[1][4] = (ImageView) findViewById(R.id.iv14);
+        mapspict[0][5] = (ImageView) findViewById(R.id.iv05);   mapspict[1][5] = (ImageView) findViewById(R.id.iv15);
+
+        mapspict[2][0] = (ImageView) findViewById(R.id.iv20);   mapspict[3][0] = (ImageView) findViewById(R.id.iv30);
+        mapspict[2][1] = (ImageView) findViewById(R.id.iv21);   mapspict[3][1] = (ImageView) findViewById(R.id.iv31);
+        mapspict[2][2] = (ImageView) findViewById(R.id.iv22);   mapspict[3][2] = (ImageView) findViewById(R.id.iv32);
+        mapspict[2][3] = (ImageView) findViewById(R.id.iv23);   mapspict[3][3] = (ImageView) findViewById(R.id.iv33);
+        mapspict[2][4] = (ImageView) findViewById(R.id.iv24);   mapspict[3][4] = (ImageView) findViewById(R.id.iv34);
+        mapspict[2][5] = (ImageView) findViewById(R.id.iv25);   mapspict[3][5] = (ImageView) findViewById(R.id.iv35);
+
+        mapspict[4][0] = (ImageView) findViewById(R.id.iv40);
+        mapspict[4][1] = (ImageView) findViewById(R.id.iv41);
+        mapspict[4][2] = (ImageView) findViewById(R.id.iv42);
+        mapspict[4][3] = (ImageView) findViewById(R.id.iv43);
+        mapspict[4][4] = (ImageView) findViewById(R.id.iv44);
+        mapspict[4][5] = (ImageView) findViewById(R.id.iv45);
+    }
+    //endregion
 }
---
+++ app/src/main/java/com/application/melanialani/fillall/Stage1.java
@@ -0,0 +1,178 @@
+package com.application.melanialani.fillall;
+
+/**
+ * Created by melan on 7/9/2016.
+ */
+public class Stage1 {
+    private int         tinggi, lebar, posX, posY;
+    private String[][]  map;
+
+    public Stage1(){}
+
+    private void level1(){
+        this.tinggi = 1;
+        this.lebar = 4;
+
+        this.map = new String[tinggi][lebar];
+
+        // empty array maps
+        for (int x = 0; x < tinggi; x++){
+            for (int y = 0; y < lebar; y++){
+                try {
+                    this.map[x][y] = "0";
+                } catch (Exception ex){
+
+                }
+            }
+        }
+
+        // set default player position
+        this.posX = 0;
+        this.posY = 0;
+        map[posX][posY] = "1";
+    }
+
+    private void level2(){
+        this.tinggi = 4;
+        this.lebar = 4;
+
+        this.map = new String[tinggi][lebar];
+
+        // empty array maps
+        for (int x = 0; x < tinggi; x++){
+            for (int y = 0; y < lebar; y++){
+                try {
+                    this.map[x][y] = "0";
+                } catch (Exception ex){
+
+                }
+            }
+        }
+
+        // set default player position
+        this.posX = 0;
+        this.posY = 0;
+        map[posX][posY] = "1";
+    }
+
+    private void level3(){
+        this.tinggi = 5;
+        this.lebar = 5;
+
+        this.map = new String[tinggi][lebar];
+
+        // empty array maps
+        for (int x = 0; x < tinggi; x++){
+            for (int y = 0; y < lebar; y++){
+                try {
+                    this.map[x][y] = "0";
+                } catch (Exception ex){
+
+                }
+            }
+        }
+
+        // set default player position
+        this.posX = 2;
+        this.posY = 2;
+        map[posX][posY] = "1";
+
+        // set blocks
+        map[0][4] = "#";
+        map[2][1] = "#";
+        map[2][3] = "#";
+        map[3][2] = "#";
+        map[4][0] = "#";
+        map[4][4] = "#";
+    }
+
+    private void level4(){
+        this.tinggi = 5;
+        this.lebar = 5;
+
+        this.map = new String[tinggi][lebar];
+
+        // empty array maps
+        for (int x = 0; x < tinggi; x++){
+            for (int y = 0; y < lebar; y++){
+                try {
+                    this.map[x][y] = "0";
+                } catch (Exception ex){
+
+                }
+            }
+        }
+
+        // set default player position
+        this.posX = 0;
+        this.posY = 4;
+        map[posX][posY] = "1";
+
+        // set blocks
+        map[0][0] = "#";
+        map[1][2] = "#";
+        map[3][1] = "#";
+        map[4][3] = "#";
+        map[4][4] = "#";
+    }
+
+    private void level5(){
+        this.tinggi = 5;
+        this.lebar = 6;
+
+        this.map = new String[tinggi][lebar];
+
+        // empty array maps
+        for (int x = 0; x < tinggi; x++){
+            for (int y = 0; y < lebar; y++){
+                try {
+                    this.map[x][y] = "0";
+                } catch (Exception ex){
+
+                }
+            }
+        }
+
+        // set default player position
+        this.posX = 0;
+        this.posY = 0;
+        map[posX][posY] = "1";
+
+        // set blocks
+        map[0][2] = "#";
+        map[1][0] = "#";
+        map[1][4] = "#";
+        map[2][0] = "#";
+        map[2][1] = "#";
+        map[3][3] = "#";
+        map[4][5] = "#";
+    }
+
+    public void setLevel(int level){
+        if (level == 1) this.level1();
+        else if (level == 2) this.level2();
+        else if (level == 3) this.level3();
+        else if (level == 4) this.level4();
+        else if (level == 5) this.level5();
+    }
+
+    public int getLebar() {
+        return lebar;
+    }
+
+    public String[][] getMap() {
+        return map;
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public int getTinggi() {
+        return tinggi;
+    }
+}
